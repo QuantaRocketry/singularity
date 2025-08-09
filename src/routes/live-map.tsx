88166c9a0f447928f76d5f88b7290b2cd964b213
Gibson431 <timmaxgibson@gmail.com>
2025-08-09 17:10:47 +1000

    Add mock live trajectory

--- src/routes/live-map.tsx
+++ src/routes/live-map.tsx
@@ -19,51 +19,89 @@ const getOffsetPosition = (startPosition: Cesium.Cartesian3, east: number, north
   return Cesium.Matrix4.multiplyByPoint(enuToFixed, offsetVector, result);
 };
 
+let cesiumViewer: Cesium.Viewer | null = null;
+let trajectoryEntity: Cesium.Entity | null = null;
+
+function updateTrajectory(newPositions: Cesium.Cartesian3[]) {
+  if (!cesiumViewer)
+    return;
+
+  if (newPositions.length < 2)
+    return;
+
+  if (!trajectoryEntity || !trajectoryEntity.polyline) {
+    trajectoryEntity = cesiumViewer.entities.add({
+      name: 'Trajectory',
+      polyline: {
+        positions: newPositions,
+        width: 5,
+        material: Cesium.Color.RED,
+        clampToGround: false
+      },
+    });
+  }
+  else {
+    const polyline = trajectoryEntity.polyline as Cesium.PolylineGraphics;
+    polyline.positions = new Cesium.ConstantProperty(newPositions);
+  }
+}
+
 function Map() {
-  const cesiumContainerRef = useRef(null);
+  const cesiumContainerRef = useRef<HTMLDivElement>(null);
+
+  // Mock trajectory
+  const [seconds, setSeconds] = useState(0);
+  const [linePositions, setLinePositions] = useState<Cesium.Cartesian3[]>([]);
 
   useEffect(() => {
-    // Ensure the container exists before initializing Cesium
     if (cesiumContainerRef.current) {
-      const viewer = new Cesium.Viewer(cesiumContainerRef.current, {
-        terrain: Cesium.Terrain.fromWorldBathymetry(),
-        timeline: false,  // Disables the timeline at the bottom
-        animation: false, // Disables the animation control in the bottom left
-      });
-
-      viewer.scene.globe.enableLighting = false;
-
-      const position = Cesium.Cartesian3.fromDegrees(149.917516, -31.148837, 330);
-
-      let linePositions = [];
-      const num_positions = 100
-      for (let i = 0; i < num_positions; i++) {
-        linePositions.push(getOffsetPosition(position, -i*2, i*1.5, (i/3)*(-i+num_positions-1)));
+      if (!cesiumViewer) {
+        cesiumViewer = new Cesium.Viewer(cesiumContainerRef.current, {
+          terrain: Cesium.Terrain.fromWorldTerrain(),
+          timeline: false,  // Disables the timeline at the bottom
+          animation: false, // Disables the animation control in the bottom left
+        });
+      }
+      else {
+        if (cesiumContainerRef.current.contains(cesiumViewer.container) === false) {
+          cesiumContainerRef.current.appendChild(cesiumViewer.container);
+        }
       }
 
-      viewer.entities.add({
-        name: 'Trajectory',
-        polyline: {
-          positions: linePositions,
-          width: 5,
-          material: Cesium.Color.RED,
-          clampToGround: false
-        },
-      });
+      cesiumViewer.scene.globe.enableLighting = false;
+
+      // Build mock positions
+      let positions = [];
+      const initial_position = Cesium.Cartesian3.fromDegrees(149.917516, -31.148837, 330);
+      const num_positions = 100
+      for (let i = 0; i < num_positions; i++) {
+        positions.push(getOffsetPosition(initial_position, -i * 2, i * 1.5, (i / 3) * (-i + num_positions - 1)));
+      }
+      setLinePositions(() => positions);
 
       const offset = new Cesium.HeadingPitchRange(
         Cesium.Math.toRadians(45),
         Cesium.Math.toRadians(-20),
         150
       );
+      cesiumViewer.camera.lookAt(initial_position, offset);
+    };
 
-      viewer.camera.lookAt(position, offset);
-
-      return () => {
-        viewer.destroy();
-      };
-    }
+    return () => { };
   }, []);
+
+  // Mock trajectory update
+  useEffect(() => {
+    if (seconds > 100)
+      return;
+
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+      updateTrajectory(linePositions.slice(0, seconds));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [seconds]);
 
   return (
     <div className="card card-bordered card-compact shadow-md flex-grow">
